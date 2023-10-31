7a2e6a36afbcc22b814d36ff676f7845aa073906
tinu bose <tinubose.mca2022@gmail.com>
2023-10-31 12:07:37 +0530

    home screen ui completed

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -1,7 +1,14 @@
 import 'package:tastequest/consts/images.dart';
+import 'package:tastequest/consts/strings.dart';
 
 const socialIconList = [icFacebookLogo, icGoogleLogo, icTwitterLogo];
 
 const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
 
 const secondSliderList = [imgSs1, imgSs2, imgSs3, imgSs4];
+
+const featuredImages1 = [imgS1, imgS2, imgS3];
+const featuredImages2 = [imgS4, imgS5, imgS6];
+
+const featuredTitles1 = [indian, chinese, italian];
+const featuredTitles2 = [american, spanish, romanian];
--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:tastequest/consts/consts.dart';
 import 'package:tastequest/consts/lists.dart';
+import 'package:tastequest/views/home_screen/components/feature_button.dart';
 import 'package:tastequest/widgets_common/home_buttons.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -110,7 +111,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                       : topSellers,
                             )),
                   ),
-                  10.heightBox,
+                  20.heightBox,
                   Align(
                       alignment: Alignment.centerLeft,
                       child: featuredCatogaries.text
@@ -118,27 +119,144 @@ class _HomeScreenState extends State<HomeScreen> {
                           .size(18)
                           .fontFamily(semibold)
                           .make()),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: featuredCatogaries.text
-                          .color(darkFontGrey)
-                          .size(18)
-                          .fontFamily(semibold)
-                          .make()),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: featuredCatogaries.text
-                          .color(darkFontGrey)
-                          .size(18)
-                          .fontFamily(semibold)
-                          .make()),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: featuredCatogaries.text
-                          .color(darkFontGrey)
-                          .size(18)
-                          .fontFamily(semibold)
-                          .make()),
+                  20.heightBox,
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(
+                          3,
+                          (index) => Column(
+                                children: [
+                                  featureButton(
+                                      icon: featuredImages1[index],
+                                      title: featuredTitles1[index]),
+                                  10.heightBox,
+                                  featureButton(
+                                      icon: featuredImages2[index],
+                                      title: featuredTitles2[index]),
+                                ],
+                              )).toList(),
+                    ),
+                  ),
+                  //featured foods
+                  20.heightBox,
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: redColor,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        featuredProduct.text.white
+                            .fontFamily(bold)
+                            .size(18)
+                            .make(),
+                        10.heightBox,
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: List.generate(
+                                6,
+                                (index) => Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Image.asset(
+                                          imgP1,
+                                          width: 150,
+                                          fit: BoxFit.cover,
+                                        ),
+                                        10.heightBox,
+                                        "Sadhya"
+                                            .text
+                                            .fontFamily(semibold)
+                                            .color(darkFontGrey)
+                                            .make(),
+                                        10.heightBox,
+                                        "250rs"
+                                            .text
+                                            .color(redColor)
+                                            .fontFamily(bold)
+                                            .size(16)
+                                            .make()
+                                      ],
+                                    )
+                                        .box
+                                        .white
+                                        .margin(const EdgeInsets.symmetric(
+                                            horizontal: 4))
+                                        .rounded
+                                        .padding(const EdgeInsets.all(8))
+                                        .make()),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      height: 150,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      itemCount: slidersList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          slidersList[index],
+                          fit: BoxFit.fitWidth,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      }),
+                  //all products section
+                  20.heightBox,
+                  GridView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 6,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                        mainAxisExtent: 300,
+                      ),
+                      itemBuilder: ((context, index) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              imgP5,
+                              height: 200,
+                              width: 200,
+                              fit: BoxFit.cover,
+                            ),
+                            10.heightBox,
+                            "chicken burger"
+                                .text
+                                .fontFamily(semibold)
+                                .color(darkFontGrey)
+                                .make(),
+                            const Spacer(),
+                            "250rs"
+                                .text
+                                .color(redColor)
+                                .fontFamily(bold)
+                                .size(16)
+                                .make()
+                          ],
+                        )
+                            .box
+                            .white
+                            .margin(const EdgeInsets.symmetric(horizontal: 4))
+                            .roundedSM
+                            .padding(const EdgeInsets.all(12))
+                            .make();
+                      }))
                 ],
               ),
             ),
